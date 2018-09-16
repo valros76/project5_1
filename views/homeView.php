7b53b62d30ae8a0cf00a20de617045dd6ddf5bec
valros76 <valerian.dufrene@gmail.com>
2018-09-16 14:49:51 +0200

    Add homeView.php - It's homepage of my site.

---
+++ views/homeView.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="templates/css/style.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+</head>
+<body>
+    <header>
+        <h1></h1>
+        <nav>
+            <ul>
+                <li>Inscription</li>
+            </ul>
+        </nav>
+    </header>
+
+    <section>
+        <article>
+            <h2>Article 1</h2>
+            <p>Contenu de l'article 1.</p>
+        </article>
+    </section>
+
+    <footer>
+        <p>Contenu du footer...</p>
+    </footer>
+</body>
+</html>
